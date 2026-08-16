3e8503602030943453d22443a83c9dd4bf404237
Michele Barberi <michelebarberi@TY_WR.station>
2026-08-16 23:42:27 +0200

    Add Xcode Cloud Flutter setup script

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+
+echo "========================================"
+echo "SMARTBET - XCODE CLOUD SETUP"
+echo "========================================"
+
+cd "$CI_PRIMARY_REPOSITORY_PATH"
+
+echo "Installazione Flutter..."
+
+git clone https://github.com/flutter/flutter.git \
+  --depth 1 \
+  -b stable \
+  "$HOME/flutter"
+
+export FLUTTER_ROOT="$HOME/flutter"
+export PATH="$FLUTTER_ROOT/bin:$PATH"
+
+echo "Flutter:"
+flutter --version
+
+echo "Recupero dipendenze..."
+flutter pub get
+
+echo "Preparazione iOS..."
+flutter precache --ios
+
+echo "========================================"
+echo "SMARTBET - SETUP COMPLETATO"
+echo "========================================"
